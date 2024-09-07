26a199e7aeb2bafd429687937e5b11e4bb487664
bumpmeup <4nozen@gmail.com>
2024-09-07 12:28:33 +0300

    Update config.py
    header for kraken

--- config.py
+++ config.py
@@ -13,4 +13,20 @@ headers = {
     'sec-ch-ua': '"Not)A;Brand";v="99", "Google Chrome";v="127", "Chromium";v="127"',
     'sec-ch-ua-mobile': '?0',
     'sec-ch-ua-platform': '"Linux"',}
-    
+
+headers_krack = {
+    'accept': '*/*',
+    'accept-language': 'en-US,en;q=0.9,ru;q=0.8,es;q=0.7',
+    'dnt': '1',
+    'origin': 'https://www.kraken.com',
+    'priority': 'u=1, i',
+    'referer': 'https://www.kraken.com/',
+    'sec-ch-ua': '"Chromium";v="128", "Not;A=Brand";v="24", "Google Chrome";v="128"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Linux"',
+    'sec-fetch-dest': 'empty',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-site': 'same-site',
+    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36',
+    'x-kraken-asset-name': 'new',
+}
